46e0f14a65e93a84941d4afeba6572aa4b9caee3
Nikita Kolankov <kolankovnikita@gmail.com>
2026-01-31 02:26:11 +0300

    Вынос токена из хука

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -35,9 +35,8 @@ function App() {
 
   const history = useHistory();
 
-
+  const token = localStorage.getItem("jwt");
   React.useEffect(() => {
-    const token = localStorage.getItem("jwt");
     if (token) {
       api
         .checkToken(token)
